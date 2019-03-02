c6b686fb16382a23fa54452f6584f962d3b32175
Kurzdor <pavloeksmidt@gmail.com>
2019-03-02 22:23:36 +0200

    Added images handler task

--- config/tasks/images.js
+++ config/tasks/images.js
@@ -1,14 +1,23 @@
-const gulp = require('gulp');
-const pump = require('pump');
-const rheniumrc = require('../../requireRheniumConfig');
+const gulp = require('gulp')
+const pump = require('pump')
+const rheniumrc = require('../requireRheniumConfig')
+const imagemin = require('gulp-imagemin')
 
-const env = process.env.NODE_ENV;
-const isDevelopment = env === 'development';
+const env = process.env.NODE_ENV
+const isDevelopment = env === 'development'
 
 gulp.task('images', function(cb) {
   return pump(
     [
       gulp.src(rheniumrc.directories.src.images),
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.optipng({ optimizationLevel: 3 }),
+        imagemin.svgo({
+          plugins: [{ removeDimensions: true }, { cleanupIDs: false }],
+        }),
+      ]),
       gulp.dest(
         isDevelopment
           ? rheniumrc.directories.dev.images
@@ -16,5 +25,5 @@ gulp.task('images', function(cb) {
       ),
     ],
     cb
-  );
-});
+  )
+})
