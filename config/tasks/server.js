64f9654a185be6c5ccfad624afd5c25a927d8fe7
Kurzdor <pavloeksmidt@gmail.com>
2019-03-02 22:23:49 +0200

    Codestyle rework

--- config/tasks/server.js
+++ config/tasks/server.js
@@ -1,8 +1,8 @@
-const gulp = require('gulp');
-const bsync = require('browser-sync');
-const rheniumrc = require('../../requireRheniumConfig');
+const gulp = require('gulp')
+const bsync = require('browser-sync')
+const rheniumrc = require('../requireRheniumConfig')
 
 gulp.task('server', function() {
-  bsync.init(rheniumrc.server.config);
-  bsync.watch(rheniumrc.server.watch).on('change', bsync.reload);
-});
+  bsync.init(rheniumrc.server.config)
+  bsync.watch(rheniumrc.server.watch).on('change', bsync.reload)
+})
