04ce64431b8ac1eb33fd612a94d6c349730ae40b
Kurzdor <pavloeksmidt@gmail.com>
2019-03-02 22:22:34 +0200

    Add index.js in template

---
+++ template/src/js/index.js
@@ -0,0 +1,12 @@
+// Start writing your JS code!
+// Happy coding!
+
+console.log(
+  `%cThis project is bootstrapped with Rhenium-CLI!`,
+  'background-color: #303033; color: #fe9920; font-size: 20px'
+)
+console.log('https://github.com/Kurzdor/rhenium-cli/')
+console.log(
+  'Docs:',
+  'https://github.com/Kurzdor/rhenium-cli/blob/master/README.md'
+)
